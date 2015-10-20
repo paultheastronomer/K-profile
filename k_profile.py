01bd871b19de28f917ebd8313f8b12be6ce28320
Paul Anthony Wilson <paultheastronomer@protonmail.com>
2015-10-20 09:58:12 +0100

    Updated the code

--- k_profile.py
+++ k_profile.py
@@ -4,110 +4,123 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 from scipy import special
 
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+def read_params():
+	with open('params/reduction.json') as param_file:    
+		param = json.load(param_file)
+	return param
+
 def voigt(x, y):
-   # The Voigt function is also the real part of 
-   # w(z) = exp(-z^2) erfc(iz), the complex probability function,
-   # which is also known as the Faddeeva function. Scipy has 
-   # implemented this function under the name wofz()
-   z = x + 1j*y
-   I = special.wofz(z).real
-   return I
+	# The Voigt function is also the real part of 
+	# w(z) = exp(-z^2) erfc(iz), the complex probability function,
+	# which is also known as the Faddeeva function. Scipy has 
+	# implemented this function under the name wofz()
+	z = x + 1j*y
+	I = special.wofz(z).real
+	return I
 
 def H(nu_,nu,c,k,T,mu_,gamma):
-  v_=nu_/c*np.sqrt(2*k*T/mu_) 	# Doppler
-  u=(nu-nu_)/(v_) 	 	# Normalized Doppler displacement
-  a=gamma/(4*np.pi*v_)	 	# Damping Constant
-  H = voigt(u,a)
-  return H,v_
+	v_=nu_/c*np.sqrt(2*k*T/mu_) 	# Doppler
+	u=(nu-nu_)/(v_) 	 	# Normalized Doppler displacement
+	a=gamma/(4*np.pi*v_)	 	# Damping Constant
+	H = voigt(u,a)
+	return H,v_
 
 def crossection(sigma_,H,v_):
-  sigma_voigt=sigma_*H/(np.sqrt(np.pi)*v_)
-  return sigma_voigt
+	sigma_voigt=sigma_*H/(np.sqrt(np.pi)*v_)
+	return sigma_voigt
 
 def lecavelier(cross_sec,k,T,mu,g,abundance,P_ref,t_eq,Rp):  # Absorption as a function of wavelength
-  H = (k*T)/(mu*g)
-  print "Scale Height: \t\t",round(H/100000.,2)," km"
-  return H*np.log(  (abundance*cross_sec*P_ref/t_eq)*np.sqrt(2.*np.pi*Rp/(k*T*mu*g))  )
+	H = (k*T)/(mu*g)
+	print "Scale Height: \t\t",round(H/100000.,2)," km"
+	return H*np.log(  (abundance*cross_sec*P_ref/t_eq)*np.sqrt(2.*np.pi*Rp/(k*T*mu*g))  )
 
-# Contants
-k=1.380658e-16 		# gm*cm^2/s^2 * Kel
-c=29979245800 		# cm/s
-me=9.10938215e-28	# electron mass cgs
-e_charge=4.80320427e-10 # electron charge
-mu = 2.35 * 1.6726e-24  # Mean molecular weight of atmosphere
+def main():    
+	param = read_params()	# Read parameter file reduction.json
 
-# Parameters
-R_sun = 6.95508e10		# Solar radius in [cm]
-Rs = 1.174*R_sun          	# Star Radius in [cm] (6.95508e10 cm is R_Sun)
-Rp = 0.11802*Rs			# Planet Radius in [cm] (7.1492e9 cm is R_Jup)
-T=1500				# K Atmospheric Temperature 1500
-g = 746.4488			# Gravity in cgs
-P_ref = 150000			# 150 mbar, Reference Pressure (Base of atmosphere) P_0 [Barye]
-t_eq = 0.56
+	# Contants
+	k		= param["constants"]["k"]	# gm*cm^2/s^2
+	c		= param["constants"]["c"]	# cm/s
+	me		= param["constants"]["me"]	# electron mass cgs
+	e_charge= param["constants"]["e"]	# electron charge
+	u 		= param["constants"]["u"]	# mean molecular weight
+	mu 		= param["system parameters"]["mu"]*u  # mean molecular mass
 
-# H2
-mu_H2=1.6726e-24*2 	# g H2 mass
+	# Parameters
+	R_sun	= param["constants"]["R_Sun"]	# Solar radius in [cm]
+	Rs		= param["system parameters"]["R*/R_Sun"]*R_sun	# Star Radius in [cm] (6.95508e10 cm is R_Sun)
+	Rp		= param["system parameters"]["Rp/R_Sun"]*Rs		# Planet Radius in [cm] (7.1492e9 cm is R_Jup)
+	T		= param["system parameters"]["temp"]			# K Atmospheric Temperature
+	g		= param["system parameters"]["g"]				# Gravity in cgs
+	P_ref	= param["system parameters"]["P_ref"]			# Reference Pressure (Base of atmosphere) P_0 [Barye]
+	t_eq 	= param["system parameters"]["t_eq"]			# Optical depth
 
-# K
-mu_K=1.6726e-24*39 	# g Na mass
-gamma_K_nu1=38.7e6 	# hz Decay Rate Na
-gamma_K_nu2=38.2e6 	# hz Decay Rate Na
-fd1_k=1.3614 		# absorption oscillator strength  - Handbook chemistry and physics
-fd2_k=0.6776 		# absorption oscillator strength
-abundK= 1.288e-7	# Potassium abundance
+	# H2
+	mu_H2	= u*2 	# g H2 mass
 
-# Wavelength & Frequency Array
-w_aa = np.arange(7682-500,7682+500,0.01) # Wavelength in Angstrom
-w_cm = w_aa*1e-8  			 # Wavelength in cm
-nu=c/w_cm 				 # Frequency
+	# K
+	mu_K	= u*39 	# g Na mass
+	gamma_K_nu1 = param["K"]["gamma_nu1"] 	# hz Decay Rate Na
+	gamma_K_nu2 = param["K"]["gamma_nu1"] 	# hz Decay Rate Na
+	fd1_k		= param["K"]["fd1"] 		# absorption oscillator strength  - Handbook chemistry and physics
+	fd2_k 		= param["K"]["fd2"] 		# absorption oscillator strength
+	abundK		= param["K"]["abund"]	# Potassium abundance
 
-S=np.pi*e_charge**2 /(me*c)
+	# Wavelength & Frequency Array
+	w_aa = np.arange(7682-500,7682+500,0.01) # Wavelength in Angstrom
+	w_cm = w_aa*1e-8  			 # Wavelength in cm
+	nu=c/w_cm 				 # Frequency
 
-sigma_k_1=S*fd1_k
-sigma_k_2=S*fd2_k
+	S=np.pi*e_charge**2 /(me*c)
 
-nu_2K=c/7699.0e-8
-nu_1K=c/7664.9e-8
+	sigma_k_1=S*fd1_k
+	sigma_k_2=S*fd2_k
 
-H_K_1,v_K_d1 = H(nu_1K,nu,c,k,T,mu_K,gamma_K_nu1)
-H_K_2,v_K_d2 = H(nu_2K,nu,c,k,T,mu_K,gamma_K_nu2)
+	nu_2K=c/param["K"]["K2"]
+	nu_1K=c/param["K"]["K1"]
 
-sigma_K_voigt1 = crossection(sigma_k_1,H_K_1,v_K_d1)
-sigma_K_voigt2 = crossection(sigma_k_2,H_K_2,v_K_d2)
+	H_K_1,v_K_d1 = H(nu_1K,nu,c,k,T,mu_K,gamma_K_nu1)
+	H_K_2,v_K_d2 = H(nu_2K,nu,c,k,T,mu_K,gamma_K_nu2)
 
-sigma_K_voigt=sigma_K_voigt1+sigma_K_voigt2 # Cross Section of Doublet
+	sigma_K_voigt1 = crossection(sigma_k_1,H_K_1,v_K_d1)
+	sigma_K_voigt2 = crossection(sigma_k_2,H_K_2,v_K_d2)
 
-cross_sec_K = sigma_K_voigt
+	sigma_K_voigt=sigma_K_voigt1+sigma_K_voigt2 # Cross Section of Doublet
 
-unbinned_high = lecavelier(cross_sec_K,k,2000.,mu,g,abundK,P_ref,t_eq,Rp)
-unbinned_med = lecavelier(cross_sec_K,k,1500.,mu,g,abundK,P_ref,t_eq,Rp)
-unbinned_low = lecavelier(cross_sec_K,k,1000.,mu,g,abundK,P_ref,t_eq,Rp)
+	cross_sec_K = sigma_K_voigt
 
-# Plot the data
-fig = plt.figure(figsize=(7,8.5/1.5))
+	unbinned_high = lecavelier(cross_sec_K,k,2000.,mu,g,abundK,P_ref,t_eq,Rp)
+	unbinned_med = lecavelier(cross_sec_K,k,1500.,mu,g,abundK,P_ref,t_eq,Rp)
+	unbinned_low = lecavelier(cross_sec_K,k,1000.,mu,g,abundK,P_ref,t_eq,Rp)
 
-fontlabel_size = 19
-tick_size = 19
-params = {'backend': 'wxAgg', 'lines.markersize' : 2, 'axes.labelsize': fontlabel_size, 'font.size': fontlabel_size, 'legend.fontsize': fontlabel_size, 'xtick.labelsize': tick_size, 'ytick.labelsize': tick_size, 'text.usetex': True}
-plt.rcParams.update(params)
-plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-ax=fig.add_subplot(1,1,1)
+	# Plot the data
+	fig = plt.figure(figsize=(7,8.5/1.5))
 
-# Profiles with various temperatures
-plt.plot(w_aa,(unbinned_high+Rp)/Rs,'-',color='red',linewidth=2.)
-plt.plot(w_aa,(unbinned_med+Rp)/Rs,'-',color='#FFBC40',linewidth=2.)
-plt.plot(w_aa,(unbinned_low+Rp)/Rs,'-',color='#375FA9',linewidth=2.)
+	fontlabel_size = 19
+	tick_size = 19
+	params = {'backend': 'wxAgg', 'lines.markersize' : 2, 'axes.labelsize': fontlabel_size, 'font.size': fontlabel_size, 'legend.fontsize': fontlabel_size, 'xtick.labelsize': tick_size, 'ytick.labelsize': tick_size, 'text.usetex': True}
+	plt.rcParams.update(params)
+	plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+	ax=fig.add_subplot(1,1,1)
 
-ax.legend(('2000~K','1500~K','1000~K'),loc='upper left', numpoints = 1)
+	# Profiles with various temperatures
+	plt.plot(w_aa,(unbinned_high+Rp)/Rs,'-',color='red',linewidth=2.)
+	plt.plot(w_aa,(unbinned_med+Rp)/Rs,'-',color='#FFBC40',linewidth=2.)
+	plt.plot(w_aa,(unbinned_low+Rp)/Rs,'-',color='#375FA9',linewidth=2.)
 
-plt.minorticks_on()
-plt.ylim(0.113,0.145)
-plt.xlim(7681-400,7681+400)
-plt.xlabel('Wavelength [\AA]')
-plt.ylabel('$R_p / R_*$')
-plt.savefig('K_profile_temp.pdf',transparent=True, bbox_inches='tight', pad_inches=0.1)
-plt.show()
+	ax.legend(('2000~K','1500~K','1000~K'),loc='upper left', numpoints = 1)
+
+	plt.minorticks_on()
+	plt.ylim(0.113,0.145)
+	plt.xlim(7681-400,7681+400)
+	plt.xlabel('Wavelength [\AA]')
+	plt.ylabel('$R_p / R_*$')
+	plt.savefig('K_profile_temp.pdf',transparent=True, bbox_inches='tight', pad_inches=0.1)
+	plt.show()
+	
+if __name__ == '__main__':
+    main()
